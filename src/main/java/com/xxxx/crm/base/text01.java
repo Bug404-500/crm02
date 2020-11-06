e6eebe672c6fb63d5a906255766cb5fb427a0014
Bug404-500 <73776612+Bug404-500@users.noreply.github.com>
2020-11-06 20:07:14 +0800

    Update text01.java

--- src/main/java/com/xxxx/crm/base/text01.java
+++ src/main/java/com/xxxx/crm/base/text01.java
@@ -3,4 +3,10 @@ package com.xxxx.crm.base;
 public class text01 {
   //我是注释1
   //woshi 注释2
+  
+   @Override
+    public String toString() {
+
+      System.out.println("啊哈哈");
+        return super.toString();
 }

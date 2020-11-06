f9d97d60141d50cd72fb4a43ddf64e668c3bd09d
错误404-500 <1548012639@qq.com>
2020-11-06 20:13:20 +0800

    提交

--- src/main/java/com/xxxx/crm/base/text01.java
+++ src/main/java/com/xxxx/crm/base/text01.java
@@ -1,12 +1,19 @@
 package com.xxxx.crm.base;
 
 public class text01 {
-  //我是注释1
-  //woshi 注释2
-  
-   @Override
+    //我是注释1
+    //woshi 注释2
+
+
+    @Override
     public String toString() {
 
-      System.out.println("啊哈哈");
+        System.out.println("啊哈哈");
         return super.toString();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hghg");
+    }
 }
